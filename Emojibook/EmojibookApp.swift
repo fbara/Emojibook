d2347951f24e92f1e1f4ed9ecd859cc9d3c41906
fbara <fbara1@yahoo.com>
2021-05-14 22:30:53 -0500

    Initial Commit

---
+++ Emojibook/EmojibookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojibookApp.swift
+//  Emojibook
+//
+//  Created by Frank Bara on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojibookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
